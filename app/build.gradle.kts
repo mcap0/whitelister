0601f393fd2539e6918dcaf79f5bb65f7eedcc1c
Whitelister <whitelister@whitelister.app>
2026-08-27 13:09:08 +0200

    chore: bump version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.whitelister.app"
         minSdk = 30
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     buildTypes {
